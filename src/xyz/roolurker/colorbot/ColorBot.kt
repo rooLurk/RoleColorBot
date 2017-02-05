3e8c74e7cb517cbab2732d94e47a2f285cb89657
rooLurk <servers@aneko.xyz>
2017-02-05 04:24:13 -0800

    Updated colors

--- src/xyz/roolurker/colorbot/ColorBot.kt
+++ src/xyz/roolurker/colorbot/ColorBot.kt
@@ -3,7 +3,10 @@ package xyz.roolurker.colorbot
 import net.dv8tion.jda.core.AccountType
 import net.dv8tion.jda.core.JDA
 import net.dv8tion.jda.core.JDABuilder
-import net.dv8tion.jda.core.entities.*
+import net.dv8tion.jda.core.entities.ChannelType
+import net.dv8tion.jda.core.entities.Guild
+import net.dv8tion.jda.core.entities.Member
+import net.dv8tion.jda.core.entities.Role
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent
 import net.dv8tion.jda.core.hooks.ListenerAdapter
 
@@ -47,6 +50,7 @@ class ColorBot(token: String) : ListenerAdapter() {
 		super.onMessageReceived(e)
 		if (e == null) {
 			return
+			//
 		} else if (e.channel.id != commandChan || e.author.isBot || e.channel.type != ChannelType.TEXT) {
 			return
 		}
@@ -54,65 +58,52 @@ class ColorBot(token: String) : ListenerAdapter() {
 		val split: List<String> = msg.split(" ", limit = 2)
 		if (split.isNotEmpty()) {
 			if ("!color" == split[0] || "!colour" == split[0]) {
-				val channel: MessageChannel = e.channel
-				val member: Member = e.member
-				if (split.size == 2) {
-					val par2: String = split[1]
-					if ("remove" == par2) {
-						val oldRole: Role? = getColorRole(member)
-						if (oldRole == null) {
-							channel.sendMessage("${member.asMention} You do not have a color!").queue()
-							return
-						} else {
-							e.guild.controller.removeRolesFromMember(member, oldRole).queue({
-								channel.sendMessage("${member.asMention} Done!").queue()
-							})
-							return
-						}
-					}
-					try {
-						val i: Int = par2.toInt()
-						val guild: Guild = e.guild
-						val hex: String? = indexToHex(i)
-						if (hex == null) {
-							channel.sendMessage("${member.asMention} Invalid index.\nUsage: `!color <index>`. See channel topic for available colors.").queue()
-						} else {
-							for (role in guild.roles) {
-								if (hex == role.name && role.permissionsRaw == 0x00000000L) {
-									addRole(guild, hex, member, channel)
-									return
-								}
-							}
-							channel.sendMessage("${member.asMention} Role could not be found, this is an issue on the guild's end.").queue()
-							return
-						}
-					} catch (e: NumberFormatException) {
-						channel.sendMessage("${member.asMention} Invalid index.\nUsage: `!color <index>`. See channel topic for available colors.").queue()
-						return
-					}
-				} else {
-					channel.sendMessage("Usage: `!color <index>`. See channel topic for available colors.").queue()
-					return
-				}
+				e.channel.sendMessage(doRoleStuff(e.guild, e.member, split)).queue()
 			}
 		}
 	}
 
-	fun addRole(guild: Guild, hex: String, member: Member, channel: MessageChannel) {
-		val oldRole: Role? = getColorRole(member)
-
-		if (oldRole != null) {
-			channel.sendMessage("${member.asMention} You already have a color! Please remove it with `!color remove` first.").queue()
-			return
-		}
-		for (i in guild.roles) {
-			if (i.name == hex) {
-				guild.controller.addRolesToMember(member, i).queue({
-					channel.sendMessage("${member.asMention} Done! Enjoy your new color.").queue()
-				})
-				return
+	fun doRoleStuff(guild: Guild, member: Member, split: List<String>): String {
+		if (split.size == 2) {
+			val par2: String = split[1]
+			if ("remove" == par2) {
+				val oldRole: Role? = getColorRole(member)
+				if (oldRole == null) {
+					return "${member.effectiveName}, you do not have a color!"
+				} else {
+					guild.controller.removeRolesFromMember(member, oldRole).queue()
+					return "${member.effectiveName}, done!"
+				}
 			}
+			try {
+				val i: Int = par2.toInt()
+				val hex: String? = indexToHex(i)
+				if (hex == null) {
+					return "${member.effectiveName}, invalid index.\nUsage: `!color <index>`. See channel topic for available colors."
+				} else {
+					guild.roles.filter { hex == it.name && it.permissionsRaw == 0x00000000L }.forEach {
+						return addRole(guild, hex, member)
+					}
+					return "${member.effectiveName}, role could not be found, this is an issue on the guild's end."
+				}
+			} catch (e: NumberFormatException) {
+				return "${member.effectiveName}, invalid index.\nUsage: `!color <index>`. See channel topic for available colors."
+			}
+		} else {
+			return "Usage: `!color <index>`. See channel topic for available colors."
 		}
+	}
+
+	fun addRole(guild: Guild, hex: String, member: Member): String {
+		val oldRole: Role? = getColorRole(member)
+		if (oldRole != null) {
+			return "${member.effectiveName}, you already have a color! Please remove it with `!color remove` first."
+		}
+		guild.roles.filter { it.name == hex }.forEach {
+			guild.controller.addRolesToMember(member, it).queue()
+			return "${member.effectiveName}, done! Enjoy your new color."
+		}
+		return "Unknown error"
 	}
 
 	fun getColorRole(member: Member): Role? {
@@ -121,56 +112,56 @@ class ColorBot(token: String) : ListenerAdapter() {
 
 	fun indexToHex(index: Int): String? {
 		when (index) {
-			1 -> return "#C1C1C1" // UPDATED from "#FFFFFF"
-			2 -> return "#010101"
-			3 -> return "#C0C0C0"
-			4 -> return "#808080"
-			5 -> return "#87CEEB"
-			6 -> return "#008FFF"
-			7 -> return "#4169E1"
-			8 -> return "#3059FF" // UPDATED from "#0033FF"
-			9 -> return "#07025E" // UPDATED from "#00058F"
-			10 -> return "#5F5FCD" // UPDATED from "#000550"
+			1 -> return "#C1C1C1"
+			2 -> return "#808080"
+			3 -> return "#010101"
+			4 -> return "#88CBE7"
+			5 -> return "#82ADE2"
+			6 -> return "#4169E1"
+			7 -> return "#3059FF"
+			8 -> return "#5F5FCD"
+			9 -> return "#5E4DD6"
+			10 -> return "#07025E"
 			11 -> return "#90FFFF"
 			12 -> return "#00FFFF"
 			13 -> return "#40E0D0"
-			14 -> return "#05C2C5"
-			15 -> return "#008080"
-			16 -> return "#00FA9A"
-			17 -> return "#00FFCC"
+			14 -> return "#00FFCC"
+			15 -> return "#00FA9A"
+			16 -> return "#05C2C5"
+			17 -> return "#008080"
 			18 -> return "#3CB371"
 			19 -> return "#1C9451"
-			20 -> return "#ADFF2F"
+			20 -> return "#3EBB2D"
 			21 -> return "#00FF00"
-			22 -> return "#3EBB2D"
+			22 -> return "#338A01"
 			23 -> return "#008000"
 			24 -> return "#A5D769"
-			25 -> return "#FAD7AE"
-			26 -> return "#EB9A3B"
-			27 -> return "#FF9000"
-			28 -> return "#CF7A27"
-			29 -> return "#C96016"
-			30 -> return "#A0522D"
-			31 -> return "#EC8F6A"
-			32 -> return "#EC7171"
-			33 -> return "#FF5A3D"
+			25 -> return "#ADFF2F"
+			26 -> return "#FAD7AE"
+			27 -> return "#FFFF00"
+			28 -> return "#FFDD00"
+			29 -> return "#E6B907"
+			30 -> return "#EB9A3B"
+			31 -> return "#CF7A27"
+			32 -> return "#FF9000"
+			33 -> return "#C96016"
 			34 -> return "#FF4500"
-			35 -> return "#FF0000"
-			36 -> return "#B00B1E" // UPDATED from "#970A04"
-			37 -> return "#DB133B"
-			38 -> return "#FFFF00"
-			39 -> return "#E6B907"
-			40 -> return "#FCAAB9"
-			41 -> return "#FF69B4"
-			42 -> return "#FF1493"
-			43 -> return "#D3138C"
-			44 -> return "#AC25B1"
-			45 -> return "#FF00FF"
-			46 -> return "#A72982" // UPDATED from "#960F96"
-			47 -> return "#A869FF"
+			35 -> return "#EC8F6A"
+			36 -> return "#EC7171"
+			37 -> return "#FF0000"
+			38 -> return "#B00B1E"
+			39 -> return "#DB133B"
+			40 -> return "#FF0060"
+			41 -> return "#FCAAB9"
+			42 -> return "#FF69B4"
+			43 -> return "#FF00FF"
+			44 -> return "#FF1493"
+			45 -> return "#D3138C"
+			46 -> return "#A72982"
+			47 -> return "#AC25B1"
 			48 -> return "#8A2BE2"
 			49 -> return "#BB55FF"
-			50 -> return "#6654E6"
+			50 -> return "#A869FF"
 		}
 		return null
 	}
